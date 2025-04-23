816eba22cd060547fb2c62f9e52066cbb3eeac21
Chris Pollitt <chris.pollitt@gmail.com>
2025-04-22 19:22:36 -0700

    misc changes

--- maze4/calltree.sh
+++ maze4/calltree.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+#
+# Generate function call tree by static code analysis
+# Requires: https://github.com/linux-sir/calltree
+#
 
 SRC="maze4.cc"
-SRC2="${SRC%.cc}-tmp.cc"
-perl -lpe 's/errno == EINTR/1/; s/#include <cerrno>/;/;' $SRC > ${SRC%.cc}-tmp.cc
 FUNCS=$(perl -ne '/^\w+ (\w+).*\) \{/ and print "$1|"' $SRC)
 FUNCS="\\b(${FUNCS%?})\\b"
-/opt/schily/bin/calltree -m $SRC2 2>/dev/null | grep -P "$FUNCS" > calltree.txt
-rm $SRC2
+/opt/schily/bin/calltree -m $SRC 2>/dev/null | grep -P "$FUNCS" > calltree.txt
 less -iS calltree.txt
